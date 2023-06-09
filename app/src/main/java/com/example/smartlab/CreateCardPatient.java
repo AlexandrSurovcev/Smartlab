6a642344fca79deaea4e099e6aba04dd4f3592af
Суровцев Александр Антонович <pr110surovcev@oat.local>
2023-06-09 12:25:42 +0600

    sixth commit

--- app/src/main/java/com/example/smartlab/CreateCardPatient.java
+++ app/src/main/java/com/example/smartlab/CreateCardPatient.java
@@ -1,25 +1,35 @@
 package com.example.smartlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.Calendar;
+
 public class CreateCardPatient extends AppCompatActivity {
     SharedPreferences preferences;
-    boolean name,patronymic,surname,dateofbirth,btnEnabled;
+    boolean name,patronymic,surname,dateofbirth,genderbool,btnEnabled;
     EnableTextView ETV = new EnableTextView();
-    String[] genders = {"Женский", "Мужской"};
     TextView btnCreate,editname,editpatronymic,editsurname,editdateofbirth;
-    Spinner gender;
+    AutoCompleteTextView genders;
+    Calendar calendar = Calendar.getInstance();
 TextView skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +37,11 @@ TextView skip;
         setContentView(R.layout.activity_create_card_patient);
         skip = findViewById(R.id.skip);
         btnCreate = findViewById(R.id.btnCreate);
-
-
-
-//спиннер
-        gender = findViewById(R.id.editgender);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, genders);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        gender.setAdapter(adapter);
+//ВЫПАДАЮЩИЙ СПИСОК
+        genders = findViewById(R.id.genders);
+        String[] gender = {"Мужской","Женский"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, R.layout.dropdown_items, gender);
+        genders.setAdapter(arrayAdapter);
 
 //поля ввода
         editname = findViewById(R.id.editname);
@@ -55,7 +62,7 @@ TextView skip;
             public void afterTextChanged(Editable editable) {
                 if(editname.getText().toString().length()!=0){
                     name=true;
-                    if(name && patronymic && surname){
+                    if(name && patronymic && surname&& dateofbirth && genderbool){
                         ETV.onEnableBtn(btnCreate,getApplicationContext());
                         btnEnabled = true;
                     }
@@ -77,7 +84,7 @@ TextView skip;
             public void afterTextChanged(Editable editable) {
                 if(editpatronymic.getText().toString().length()!=0){
                     patronymic=true;
-                    if(name && patronymic && surname){
+                    if(name && patronymic && surname&& dateofbirth && genderbool){
                         ETV.onEnableBtn(btnCreate,getApplicationContext());
                         btnEnabled = true;
                     }
@@ -99,7 +106,7 @@ TextView skip;
             public void afterTextChanged(Editable editable) {
                 if(editsurname.getText().toString().length()!=0){
                 surname=true;
-                if(name && patronymic && surname ){
+                if(name && patronymic && surname&& dateofbirth && genderbool ){
                     ETV.onEnableBtn(btnCreate,getApplicationContext());
                     btnEnabled = true;
                 }
@@ -110,20 +117,92 @@ TextView skip;
                 }
             }
         });
+        editdateofbirth.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editdateofbirth.getText().toString().length()!=0){
+                    dateofbirth=true;
+                    editdateofbirth.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.black));
+                    if(name && patronymic && surname&& dateofbirth && genderbool){
+                        ETV.onEnableBtn(btnCreate,getApplicationContext());
+                        btnEnabled = true;
+                    }
+                }
+                else {
+                    ETV.onDisableBtn(btnCreate,getApplicationContext());
+                    btnEnabled = false;
+                }
+            }
+        });
+        genders.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(genders.getText().toString().length()!=0){
+                    genderbool=true;
+                    genders.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.black));
+                    if(name && patronymic && surname&& dateofbirth && genderbool){
+                        ETV.onEnableBtn(btnCreate,getApplicationContext());
+                        btnEnabled = true;
+                    }
+                }
+                else {
+                    ETV.onDisableBtn(btnCreate,getApplicationContext());
+                    btnEnabled = false;
+                }
+            }
+        });
+
 
 
     }
+    //ДАТА
+    public void getdateBtn(View view){
+        new DatePickerDialog(CreateCardPatient.this,d,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+    // установка начальных даты
+    private void setInitialDate() {
+
+        editdateofbirth.setText(DateUtils.formatDateTime(this,
+                calendar.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR));
+    }
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDate();
+        }
+    };
+
     public void onCreate(View view){
         if(btnEnabled){
             String nameValue = editname.getText().toString();
             String patronymicValue = editpatronymic.getText().toString();
             String surnameValue = editsurname.getText().toString();
+            String dateValue = editdateofbirth.getText().toString();
+            String genderValue = genders.getText().toString();
             SharedPreferences.Editor editor = preferences.edit();
             editor.putString("name", nameValue);
             editor.putString("patronymic", patronymicValue);
             editor.putString("surname",surnameValue);
-//            editor.putString("dateofbirth",dateValue);
-//            editor.putString("gender",true);
+            editor.putString("date",dateValue);
+            editor.putString("gender",genderValue);
             editor.apply();
             Intent intent = new Intent(CreateCardPatient.this,ActivityMenu.class);
             startActivity(intent);
--- app/src/main/java/com/example/smartlab/Profile.java
+++ app/src/main/java/com/example/smartlab/Profile.java
@@ -3,21 +3,37 @@ package com.example.smartlab;
 import static com.example.smartlab.R.layout.fragment_analys;
 import static com.example.smartlab.R.layout.fragment_profile;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class Profile extends Fragment {
     SharedPreferences preferences;
-    TextView btnCreate,editname,editpatronymic,editsurname,editdateofbirth;
+    TextView btnSave,editname,editpatronymic,editsurname,editdateofbirth;
+    boolean name,patronymic,surname,dateofbirth,genderbool,btnEnabled;
+    AutoCompleteTextView genders;
+    EnableTextView ETV = new EnableTextView();
+    Calendar calendar = Calendar.getInstance();
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(fragment_profile, container, false);
@@ -26,10 +42,179 @@ public class Profile extends Fragment {
         editpatronymic = v.findViewById(R.id.editpatronymic);
         editsurname = v.findViewById(R.id.editsurname);
         editdateofbirth = v.findViewById(R.id.editdateofbirth);
+        //ВЫПАДАЮЩИЙ СПИСОК
+        genders = v.findViewById(R.id.genders);
+        String[] gender = {"Мужской","Женский"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(getContext(), R.layout.dropdown_items, gender);
+        genders.setAdapter(arrayAdapter);
+        //ПОЛЯ ВВОДА
         preferences = getActivity().getSharedPreferences("UserInfo", 0);
         editname.setText(preferences.getString("name",null));
         editpatronymic.setText(preferences.getString("patronymic",null));
         editsurname.setText(preferences.getString("surname",null));
+        editdateofbirth.setText(preferences.getString("date",null));
+        genders.setText(preferences.getString("gender",null));
+
+        //Кнопка сохранить начальное состояние
+        if(name && patronymic && surname && dateofbirth && genderbool){
+            ETV.onEnableBtn(btnSave,getContext());
+            btnEnabled = true;
+        }
+
+        editname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editname.getText().toString().length()!=0){
+                    name=true;
+                    if(name && patronymic && surname && dateofbirth && genderbool){
+                        ETV.onEnableBtn(btnSave,getContext());
+                        btnEnabled = true;
+                    }
+                }
+                else {
+                    ETV.onDisableBtn(btnSave,getContext());
+                    btnEnabled = false;
+                }
+            }
+        });
+        editpatronymic.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editpatronymic.getText().toString().length()!=0){
+                    patronymic=true;
+                    if(name && patronymic && surname && dateofbirth && genderbool){
+                        ETV.onEnableBtn(btnSave,getContext());
+                        btnEnabled = true;
+                    }
+                }
+                else {
+                    ETV.onDisableBtn(btnSave,getContext());
+                    btnEnabled = false;
+                }
+            }
+        });
+        editsurname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editsurname.getText().toString().length()!=0){
+                    surname=true;
+                    if(name && patronymic && surname && dateofbirth && genderbool ){
+                        ETV.onEnableBtn(btnSave,getContext());
+                        btnEnabled = true;
+                    }
+                }
+                else {
+                    ETV.onDisableBtn(btnSave,getContext());
+                    btnEnabled = false;
+                }
+            }
+        });
+        editdateofbirth.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editdateofbirth.getText().toString().length()!=0){
+                    dateofbirth=true;
+                    editdateofbirth.setTextColor(ContextCompat.getColor(getContext(),R.color.black));
+                    if(name && patronymic && surname&& dateofbirth && genderbool){
+                        ETV.onEnableBtn(btnSave,getContext());
+                        btnEnabled = true;
+                    }
+                }
+                else {
+                    ETV.onDisableBtn(btnSave,getContext());
+                    btnEnabled = false;
+                }
+            }
+        });
+        genders.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(genders.getText().toString().length()!=0){
+                    genderbool=true;
+                    genders.setTextColor(ContextCompat.getColor(getContext(),R.color.black));
+                    if(name && patronymic && surname && dateofbirth && genderbool){
+                        ETV.onEnableBtn(btnSave,getContext());
+                        btnEnabled = true;
+                    }
+                }
+                else {
+                    ETV.onDisableBtn(btnSave,getContext());
+                    btnEnabled = false;
+                }
+            }
+        });
+
+
         return v;
+    }
+    //ДАТА
+    public void getdateBtn(View view){
+        new DatePickerDialog(getActivity(),d,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+    // установка начальных даты
+    private void setInitialDate() {
+
+        editdateofbirth.setText(DateUtils.formatDateTime(getActivity(),
+                calendar.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR));
+    }
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDate();
+        }
+    };
+    public void onSave(View view){
+        if(btnEnabled){
+            String nameValue = editname.getText().toString();
+            String patronymicValue = editpatronymic.getText().toString();
+            String surnameValue = editsurname.getText().toString();
+            String dateValue = editdateofbirth.getText().toString();
+            String genderValue = genders.getText().toString();
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("name", nameValue);
+            editor.putString("patronymic", patronymicValue);
+            editor.putString("surname",surnameValue);
+            editor.putString("date",dateValue);
+            editor.putString("gender",genderValue);
+            editor.apply();
+            Toast.makeText(getContext(),"Успешно нахуй",Toast.LENGTH_SHORT).show();
+        }
+        else  Toast.makeText(getContext(),"Не все поля заполнены",Toast.LENGTH_SHORT).show();
     }
 }
